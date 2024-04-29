9b5545fc553abae4540c3d6ff795c57b2f2abfef
Iam4ealDamba <prayforsouls720@gmail.com>
2024-04-29 12:21:40 +0200

    ajout classe bonjour

---
+++ org/iamdamba/learn_java/Bonjour.java
@@ -0,0 +1,13 @@
+package org.iamdamba.learn_java;
+
+public class Bonjour {
+  /**
+   * Methode principale de l'application
+   * 
+   * @param args - tableau de parametres : {@code String[]}
+   */
+  public static void main(String[] args) {
+    String world = "World!";
+    System.out.println("hello" + world);
+  }
+}
